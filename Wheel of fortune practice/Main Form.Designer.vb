2fd03c2cc3a5bbc3ecf8c62fa2faba03ebb5613d
egibson5977 <egibson5977@W10-CRM2CS3>
2023-04-26 13:51:04 -0500

    Add project files.

---
+++ Wheel of fortune practice/Main Form.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(802, 323)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(158, 114)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(869, 304)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Wheel_of_fortune_practice.My.Resources.Resources.chartbase
+        Me.PictureBox1.Location = New System.Drawing.Point(69, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(758, 526)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(849, 100)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 166)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(972, 550)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "frmMain"
+        Me.Text = "Wheel of Fortune"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Wheel of fortune practice/Main Form.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Security.Cryptography
+
+Public Class frmMain
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' Initialize the random-number generator.
+        Randomize()
+        ' Generate random value between 1 and 6.
+        Dim value As Integer = CInt(Int((25 * Rnd()) + 1))
+
+
+
+        Label1.Text = value
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'PictureBox1.Image = My.Resources.chartbase
+        ' Initialize the random-number generator.
+        Randomize()
+        ' Generate random value between 1 and 6.
+        Dim value As Integer = CInt(Int((25 * Rnd()) + 1))
+
+
+
+        Label1.Text = value
+
+        Dim intPic = 0
+        MsgBox(value)
+
+
+
+        For Spins As Integer = 0 To value
+            System.Threading.Thread.Sleep(5000)
+            intPic = Spins Mod 25
+            If intPic = 0 Then
+                PictureBox1.Image = My.Resources.chart1real
+            ElseIf intPic = 1 Then
+                PictureBox1.Image = My.Resources.chart2
+            ElseIf intPic = 2 Then
+                PictureBox1.Image = My.Resources.chart3
+            ElseIf intPic = 3 Then
+                PictureBox1.Image = My.Resources.chart4
+            ElseIf intPic = 4 Then
+                PictureBox1.Image = My.Resources.chart5
+            ElseIf intPic = 5 Then
+                PictureBox1.Image = My.Resources.chart6
+            ElseIf intPic = 6 Then
+                PictureBox1.Image = My.Resources.chart7
+            ElseIf intPic = 7 Then
+                PictureBox1.Image = My.Resources.chart8
+            ElseIf intPic = 8 Then
+                PictureBox1.Image = My.Resources.chart9
+            ElseIf intPic = 9 Then
+                PictureBox1.Image = My.Resources.chart10
+            ElseIf intPic = 10 Then
+                PictureBox1.Image = My.Resources.chart11
+            ElseIf intPic = 11 Then
+                PictureBox1.Image = My.Resources.chart12
+            ElseIf intPic = 12 Then
+                PictureBox1.Image = My.Resources.chart13
+            ElseIf intPic = 13 Then
+                PictureBox1.Image = My.Resources.chart14
+            ElseIf intPic = 14 Then
+                PictureBox1.Image = My.Resources.chart15
+            ElseIf intPic = 15 Then
+                PictureBox1.Image = My.Resources.chart16
+            ElseIf intPic = 16 Then
+                PictureBox1.Image = My.Resources.chart17
+            ElseIf intPic = 17 Then
+                PictureBox1.Image = My.Resources.chart18
+            ElseIf intPic = 18 Then
+                PictureBox1.Image = My.Resources.chart19
+            ElseIf intPic = 19 Then
+                PictureBox1.Image = My.Resources.chart20
+            ElseIf intPic = 20 Then
+                PictureBox1.Image = My.Resources.chart21
+            ElseIf intPic = 21 Then
+                PictureBox1.Image = My.Resources.chart22
+            ElseIf intPic = 22 Then
+                PictureBox1.Image = My.Resources.chart23
+            ElseIf intPic = 23 Then
+                PictureBox1.Image = My.Resources.chart24
+            ElseIf intPic = 24 Then
+                PictureBox1.Image = My.Resources.chart25
+
+            End If
+        Next
+
+
+
+    End Sub
+End Class
